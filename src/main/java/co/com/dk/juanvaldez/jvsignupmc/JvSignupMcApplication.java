2453e46d9f0192862599eadeebb7b97ec027e160
Miguel Morales <miguelmorales220794@gmail.com>
2022-04-26 21:35:21 -0500

    Se crea proyecto Java Spring desde cero.

---
+++ src/main/java/co/com/dk/juanvaldez/jvsignupmc/JvSignupMcApplication.java
@@ -0,0 +1,13 @@
+package co.com.dk.juanvaldez.jvsignupmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvSignupMcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvSignupMcApplication.class, args);
+	}
+
+}
